d57ccc961609c0590cdb6378c41f090e83675345
AlexRedEye <juniormarquez745@gmail.com>
2022-06-29 02:06:47 -0400

    update

--- blackjack.js
+++ blackjack.js
@@ -1,11 +1,15 @@
 let cards = []
+let dealerCards = []
 let sum = 0
+let dealerSum = 0
 let hasBlackjack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let dealerCardsEl = document.getElementById("dealerCards-el")
+let dealerSumEl = document.getElementById("dealerSum-el")
 
 let player = {
     name: "You",
@@ -35,20 +39,32 @@ function startGame()
     isAlive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
+    let dealerFirstCard = getRandomCard()
+    let dealerSecondCard = getRandomCard()
     cards = [firstCard, secondCard]
+    dealerCards = [dealerFirstCard, dealerSecondCard]
     sum = firstCard + secondCard
+    dealerSum = dealerFirstCard + dealerSecondCard
     renderGame();
 }
 
 function renderGame()
 {
     cardsEl.textContent = "Cards: "
+    dealerCardsEl.textContent = "Dealer Cards: "
+
     for (let i = 0; i < cards.length; i++)
     {
         cardsEl.textContent += cards[i] + " "
     }
 
+    for (let v = 0; v < dealerCards.length; v++)
+    {
+        dealerCardsEl.textContent += dealerCards[v] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
+    dealerSumEl.textContent = "Dealer Sum: " + dealerSum
     if (sum <= 20)
     {
         message = "Do you want to draw a new card?"
