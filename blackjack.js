3dad7636b4b212f10d5f4abd276c60a5aabcef84
AlexRedEye <juniormarquez745@gmail.com>
2022-06-29 00:59:51 -0400

    I FIXED IT

--- blackjack.js
+++ blackjack.js
@@ -7,10 +7,17 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "You",
+    chips: 200
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard()
 {
-    let randomNumber = Math.floor(Math.random() * 12) + 1;
-    return randomNumber
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
     if (randomNumber > 10)
     {
         return 10
@@ -28,13 +35,20 @@ function startGame()
     isAlive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
-    cards.push(firstCard, secondCard)
+    cards = [firstCard, secondCard]
     sum = firstCard + secondCard
     renderGame();
 }
 
 function renderGame()
 {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++)
+    {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20)
     {
         message = "Do you want to draw a new card?"
@@ -47,12 +61,7 @@ function renderGame()
         isAlive = false
     }
 
-        messageEl.textContent = message
-        sumEl.textContent = "Sum: " + sum
-        for (let i = cards.length; i <= cards.length; i++)
-        {
-            cardsEl.textContent = "Cards: " + cards
-        }
+    messageEl.textContent = message
 }
 
 function newCard()
@@ -60,8 +69,8 @@ function newCard()
     if (isAlive === true && hasBlackjack === false)
     {
         let newCard = getRandomCard()
-    sum += newCard
-    cards.push(newCard)
-    renderGame()
+        sum += newCard
+        cards.push(newCard)
+        renderGame()
     }
 }
