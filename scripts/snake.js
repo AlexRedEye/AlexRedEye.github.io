1f33b54704bc66dbca3136463031a3ebd58c124a
AlexRedEye <54732106+AlexRedEye@users.noreply.github.com>
2022-07-14 07:04:31 -0400

    yes

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,22 +1,3 @@
-const fs = require('fs');
-
-fs.readFile('./scores.json', 'utf-8', (err, jsonString) => {
-    if (err) {
-      console.log(err);
-    } else {
-        const data = JSON.parse(jsonString);
-      console.log(jsonString.Alex);
-    }
-});
-
-scoreUrl = 'http://127.0.0.1:5500/snake.html'
-async function getScore() {
-    const response = await fetch(scoreUrl);
-    const data = await response.json();
-}
-
-getScore();
-
 //board
 var blockSize = 25;
 var rows = 20;
